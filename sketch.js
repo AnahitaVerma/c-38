f95df0f3b77a141b3063cc8573f1019f7196ec19
AnahitaVerma <71075029+AnahitaVerma@users.noreply.github.com>
2021-02-11 12:06:43 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+
+var car,carImg
+
+
+function setup() {
+  createCanvas(800, 800);
+  database = firebase.database();
+
+  carImg = loadImage("images/car.png")
+  
+
+
+}
+function draw() {
+  background("black");
+ 
+}
